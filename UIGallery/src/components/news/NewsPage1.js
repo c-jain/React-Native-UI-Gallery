33bb3aa2bfe493d1f2b791c04e06ab97f4a807b2
Chakshu Jain <chakshujain1997@gmail.com>
2020-12-13 23:26:37 +0530

    News UI added

--- UIGallery/src/components/news/NewsPage1.js
+++ UIGallery/src/components/news/NewsPage1.js
@@ -1,12 +1,69 @@
 // React imports
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image } from "react-native";
+// Assets import 
+import nasa from "../../assets/News/nasa.jpg";
+// Dimension utility imports
+import { windowWidth, windowHeight } from "../../utils/Dimensions";
 
 class NewsPage1 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {id: "2"},
+                {id: "3"},
+                {id: "1"},
+                {id: "4"},
+                {id: "5"},
+            ]
+        }
+    }
+
+    renderItem = ({ item }) => {
+        return (
+            <TouchableOpacity 
+                style={styles.card}
+                onPress={() => {}}
+            >
+                <Image source={nasa} style={{width:100, height:100}}/>
+                <View style={styles.textContainer}>
+                    <Text style={styles.textHead}>Curiosity rover finds clay on mars: NASA.</Text>
+                    <Text style={styles.textSubHead}>NASA's curiosity Mars rover has found the heighest amount of clay... </Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+
     render() {
         return (
             <View style={styles.mainView}>
-                <Text>NewsPage1</Text>
+                {/* Header */}
+                <View style={styles.header}>
+                    <Text style={styles.headerText}>
+                        Daily Bungle
+                    </Text>
+                </View>
+                
+                {/* Navbar */}
+                <View style={styles.nav}>
+                    <TouchableOpacity>
+                        <Text style={styles.navText}>General</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={styles.navText}>Technology</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={styles.navText}>Business</Text>
+                    </TouchableOpacity>
+                </View>
+
+                {/* Content */}
+                <FlatList 
+                    data={this.state.data}
+                    renderItem={this.renderItem}
+                    keyExtractor={item => item.id}
+                />
             </View>
         );
     }
@@ -15,8 +72,56 @@ class NewsPage1 extends Component {
 const styles = StyleSheet.create({
     mainView: {
         flex: 1,
+    },
+    header: {
+        width: "100%",
+        height: windowHeight / 10,
         justifyContent: "center",
+        alignItems: "center",
+        padding: 5,
+        backgroundColor: "#226b3c"
+    },
+    headerText: {
+        fontSize: 28,
+        fontWeight: "700",
+        color: "white"
+    },
+    nav: {
+        width: "100%",
+        height : windowHeight / 15,
+        flexDirection: "row",
+        backgroundColor: "#2e8f50",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        padding: 5,
+    },
+    navText: {
+        fontSize: 14,
+        fontWeight: "700",
+        color: "white"
+    },
+    card: {
+        width: "100%",
+        height: 100,
+        flexDirection: "row",
+        marginBottom: 5,
+        padding: 5
+    },
+    textContainer: {
+        flex: 1,
+        padding: 5,
+        marginLeft: 5,
+        justifyContent: "space-evenly",
         alignItems: "center"
+    },
+    textHead: {
+        fontSize: 16,
+        fontWeight: "700",
+    },
+    textSubHead: {
+        fontSize: 14,
+        fontWeight: "600",
+        color: "grey"
     },
 });
 
--- UIGallery/src/components/news/NewsPage2.js
+++ UIGallery/src/components/news/NewsPage2.js
@@ -1,13 +1,30 @@
 // React imports
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+// Assets import 
+import nasa from "../../assets/News/nasa.jpg";
+// Dimension utility imports
+import { windowWidth, windowHeight } from "../../utils/Dimensions";
 
 class NewsPage2 extends Component {
     render() {
         return (
-            <View style={styles.mainView}>
-                <Text>NewsPage2</Text>
-            </View>
+            <ScrollView style={styles.mainView}>
+                {/* Heading */}
+                <View style={styles.heading}>
+                    <Text style={styles.headingText}>Curiosity rover finds clay on mars: NASA</Text>
+                </View>
+
+                {/* Image */}
+                <View style={styles.imageContainer}>
+                    <Image source={nasa} style={styles.image}/>
+                </View>
+
+                {/* Content */}
+                <View style={styles.content}>
+                    <Text style={styles.contentText}>{`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`}</Text>
+                </View>
+            </ScrollView>
         );
     }
 }
@@ -15,8 +32,39 @@ class NewsPage2 extends Component {
 const styles = StyleSheet.create({
     mainView: {
         flex: 1,
+    },
+    heading: {
+        width: "100%",
+        height: windowHeight / 5,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        padding: 5,
+    },
+    headingText: {
+        fontWeight: "700",
+        fontSize: 28
+    },
+    imageContainer: {
+        padding: 5,
+        width: "100%",
+        
+    },
+    image: {
+        width: "100%",
+        height: windowHeight / 3
+    },
+    content: {
+        width: "100%",
+        padding: 5,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 5,
+        marginBottom: 5,
+    },
+    contentText: {
+        fontSize: 18,
+        fontWeight: "500",
+        textAlign: "left"
     },
 });
 
